4a082053bb9c73b2e88c579c5a605487af1843c1
Mark Goldwater <mgoldwater@olin.edu>
2021-02-27 17:18:14 -0500

    Fixed SVD

--- SVD_simulation.m
+++ SVD_simulation.m
@@ -1,11 +1,10 @@
 function [x1_err, x2_err, x3_err, x4_err] = SVD_simulation(x_train, x_data)
     % TODO
-    
-    [H, ~] = estimate_channel(x_train);
-    [U, S, V] = svd(H);
-    x_tilda = V'*x_data;
+    [H,~] = estimate_channel(x_train);
+    [U,S,V] = svd(H);
+    x_tilda = V*x_data;
     received_data = MIMOChannel4x4(x_tilda);
-    y = U*received_data;
+    y = (U'*received_data)./(ones(size(received_data)).*diag(S));
     x1_err = compute_error(y(1,:), x_data(1,:));
     x2_err = compute_error(y(2,:), x_data(2,:));
     x3_err = compute_error(y(3,:), x_data(3,:));
